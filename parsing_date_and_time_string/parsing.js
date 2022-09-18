d9965727da4caecdd736481beecf73a924128ca0
angiee05 <cs.neparidze@gmail.com>
2022-09-18 13:21:06 +0200

    Capitalised, Fixed spaces

--- parsing_date_and_time_string/parsing.js
+++ parsing_date_and_time_string/parsing.js
@@ -1,47 +1,47 @@
 const moment = require("moment/moment")
 
-// parsing by date and time
 
+// Parsing by Current date and time
 let m = moment();
 
 console.log("Current date and time:")
 console.log(`ToString() => ${m.toString()}`)
 console.log(`ToISOString() => ${m.toISOString()}`)
 
-// Create from ISO 8601 string
 
+// Create from ISO 8601 string
 m = moment("2022-09-18T12:42:00.000+05:00")
 
 console.log("Current date and time from ISO 8601:")
 console.log(`ToString() => ${m.toString()}`)
 console.log(`ToISOString() => ${m.toISOString()}`)
 
-// Using a format 
 
+// Create Using a format 
 m = moment(moment(),"DD/MM/YYYY h:mm")
 
 console.log("Current date and time using a format:")
 console.log(`ToString() => ${m.toString()}`)
 console.log(`ToISOString() => ${m.toISOString()}`)
 
-// Create using milliseconds from epoch (1 Jan 1970)
 
+// Create using milliseconds from epoch (1 Jan 1970)
 m = moment(600000)
 
 console.log("Current date and time using milliseconds from epoch 1970:")
 console.log(`ToString() => ${m.toString()}`)
 console.log(`ToISOString() => ${m.toISOString()}`)
 
-// Create using seconds from epoch (1 Jan 1970)
 
+// Create using seconds from epoch (1 Jan 1970)
 m = moment.unix(7200)
 
 console.log("Current date and time using seconds from epoch 1970:")
 console.log(`ToString() => ${m.toString()}`)
 console.log(`ToISOString() => ${m.toISOString()}`)
 
-// Create moment object in UTC mode
 
+// Create moment object in UTC mode
 m = moment.utc("2022-09-18T13:05:00.000+05:00")
 
 console.log("Current date and time using utc mode:")
