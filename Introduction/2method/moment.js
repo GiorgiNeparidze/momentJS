e923d5c1f3ed9aaf9c913e085c94fc9019d202b3
angiee05 <cs.neparidze@gmail.com>
2022-09-18 11:55:05 +0200

    Introduction

---
+++ Introduction/2method/moment.js
@@ -0,0 +1,7 @@
+// 1) Install using npm as any other liberary npm i moment --save
+
+// 2) Import moment!
+const moment = require("moment/moment");
+
+// 3) Make sure that it works!
+console.log(moment());
